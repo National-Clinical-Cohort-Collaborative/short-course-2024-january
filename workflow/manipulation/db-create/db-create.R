367df1dfdfef005a2a76f8544036c95178265a73
Will Beasley <wibeasley@hotmail.com>
2023-08-14 17:44:04 -0500

    structure to for db ddl
    ref #1

---
+++ workflow/manipulation/db-create/db-create.R
@@ -0,0 +1,64 @@
+# knitr::stitch_rmd(script="manipulation/simulation/simulate-mlm-1.R", output="stitched-output/manipulation/simulation/simulate-mlm-1.md") # dir.create("stitched-output/manipulation/simulation", recursive=T)
+rm(list = ls(all.names = TRUE)) # Clear the memory of variables from previous run. This is not called by knitr, because it's above the first chunk.
+
+# ---- load-sources ------------------------------------------------------------
+# Call `base::source()` on any repo file that defines functions needed below.  Ideally, no real operations are performed.
+
+# ---- load-packages -----------------------------------------------------------
+# Attach these packages so their functions don't need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+# library("ggplot2")
+
+# Import only certain functions of a package into the search path.
+# import::from("magrittr", "%>%")
+
+# Verify these packages are available on the machine, but their functions need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+# requireNamespace("readr"        )
+# requireNamespace("tidyr"        )
+# requireNamespace("dplyr"        ) # Avoid attaching dplyr, b/c its function names conflict with a lot of packages (esp base, stats, and plyr).
+# requireNamespace("rlang"        ) # Language constructs, like quosures
+# requireNamespace("testit"       ) # For asserting conditions meet expected patterns/conditions.
+requireNamespace("checkmate"    ) # For asserting conditions meet expected patterns/conditions. # remotes::install_github("mllg/checkmate")
+# requireNamespace("DBI"          ) # Database-agnostic interface
+requireNamespace("RSQLite"      ) # Lightweight database for non-PHI data.
+# requireNamespace("OuhscMunge"   ) # remotes::install_github(repo="OuhscBbmc/OuhscMunge")
+
+# ---- declare-globals ---------------------------------------------------------
+# Constant values that won't change.
+config                         <- config::get()
+
+path_ddl <-
+  c(
+    "manipulation/db-create/person-drop.sql",
+    "manipulation/db-create/person-create.sql"
+  )
+
+execute_sql <- function(path_sql) {
+  checkmate::assert_file_exists(path_sql)
+
+  path_sql |>
+    readr::read_file() |>
+    DBI::dbExecute(
+      conn        = cnn,
+      statement   = _
+    )
+}
+
+# ---- load-data ---------------------------------------------------------------
+
+# sql <- readr::read_file(path_ddl)
+
+# ---- tweak-data --------------------------------------------------------------
+
+cnn <- DBI::dbConnect(RSQLite::SQLite(), dbname = config$path_database)
+# result <- DBI::dbSendQuery(cnn, "PRAGMA foreign_keys=ON;") #This needs to be activated each time a connection is made. #http://stackoverflow.com/questions/15301643/sqlite3-forgets-to-use-foreign-keys
+# DBI::dbClearResult(result)
+DBI::dbListTables(cnn)
+
+path_ddl |>
+  purrr::walk(~execute_sql(.))
+
+# Allow database to optimize its internal arrangement
+DBI::dbExecute(cnn, "VACUUM;")
+
+# Close connection
+DBI::dbDisconnect(cnn)
---
+++ workflow/manipulation/db-create/person-create.sql
@@ -0,0 +1,21 @@
+-- DROP TABLE if exists person;
+CREATE TABLE person (
+  person_id                       integer,
+  gender_concept_id               integer,
+  year_of_birth                   integer,
+  month_of_birth                  integer,
+  day_of_birth                    integer,
+  birth_datetime                  text,
+  race_concept_id                 integer,
+  ethnicity_concept_id            integer,
+  location_id                     integer,
+  provider_id                     integer,
+  care_site_id                    integer,
+  person_source_value             text,
+  gender_source_value             text,
+  gender_source_concept_id        integer,
+  race_source_value               text,
+  race_source_concept_id          integer,
+  ethnicity_source_value          text,
+  ethnicity_source_concept_id     integer
+);
