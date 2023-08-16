a48d6bb54aa236a03c195d2c16afc3fcd3415115
Will Beasley <wibeasley@hotmail.com>
2023-08-15 19:35:45 -0500

    switch to date centers
    ref #1

--- workflow/manipulation/sandbox/cases-per-day.R
+++ workflow/manipulation/sandbox/cases-per-day.R
@@ -8,37 +8,37 @@
 
 base_size <- 1000
 
-w2020_3 <- rnorm(n = base_size * 1, mean = 90, sd = 30)
-w2020_6 <- rnorm(n = base_size * 1, mean = 150, sd = 30)
-
-
-d <-
-  c(
-    w2020_3,
-    w2020_6
-  ) |>
-  tibble::tibble(day = _)
-
 f <- function(center , amplitude, sd) {
-  # browser()
-  # tibble::tibble(
-  #
-  # )
-  # as.Date("2020-01-01") +
-  rnorm(n = base_size * amplitude, mean = center, sd = sd) |>
-    as.integer() #|>
-    # lubridate::days()
+  m <- as.integer(difftime(center, as.Date("2020-01-01"), units = "day"))
+  rnorm(n = base_size * amplitude, mean = m, sd = sd) |>
+    as.integer()
 }
+
 d <-
   {
     tibble::tribble(
-      ~center , ~amplitude, ~sd,
-      94L ,         1L,   10L,
-      186L ,        2L,   10L,
-      315L ,        2L,   10L,
-      330L ,        2L,   10L,
-      345L ,        4L,   10L,
-     ) |>
+      ~center     , ~amplitude,   ~sd,
+      "2020-04-04",         1L,   20L,
+      "2020-07-05",         2L,   20L,
+      "2020-09-18",         1L,   40L,
+      "2020-11-11",         2L,   10L,
+      "2020-11-26",         2L,   10L,
+      "2020-12-11",         4L,   10L,
+      "2021-02-24",         1L,   30L,
+      "2021-04-11",         1L,   30L,
+      "2021-08-25",         2L,   15L,
+      "2021-09-04",         2L,   25L,
+      "2021-11-15",         1L,   25L,
+      "2021-11-28",         1L,   25L,
+      "2021-12-01",         1L,   15L,
+      "2022-01-01",         7L,   10L,
+      "2022-01-07",         5L,   10L,
+      "2022-03-18",         1L,   40L,
+      "2022-05-20",         1L,   20L,
+      "2022-06-20",         1L,   20L,
+      "2022-07-22",         1L,   20L,
+      "2022-12-05",         2L,   40L,
+    ) |>
     purrr::pmap(f) |>
     unlist() +
     as.Date("2020-01-01")
