3673edf9a2ff187d3d15c78a858b0f05e169a2e7
Will Beasley <wibeasley@hotmail.com>
2024-01-17 13:30:09 -0600

    start ferry
    ref #9

--- workflow/manipulation/ferry.sql
+++ workflow/manipulation/ferry.sql
@@ -1,3 +1,16 @@
 --TODO
 -- 1. create (maybe temp) table that's the last animal observation before covid DX
 -- 2. join to LL patient table
+SELECT
+  p.person_id
+  ,*
+  ,row_number() over (partition by p.person_id order by o.observation_date desc) as index_within_pt
+FROM patient p
+  inner join observation o on p.person_id = o.person_id
+WHERE
+  o.observation_date < p.covid_date
+  and
+  o.observation_concept_id in (
+    4314094,
+    4314097
+  )
